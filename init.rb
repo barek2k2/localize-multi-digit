fbe0c4922929c0083d552003808e61edc1cd13f5
Abdul Barek <barek2k2@yahoo.com>
2012-01-08 22:35:11 +0600

    i18n depended

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-require "#{Rails.root}/vendor/plugins/multi-digit/lib/core.rb"
+require "#{Rails.root}/vendor/plugins/localize-multi-digit/lib/core.rb"
 
 [String, Fixnum, Float, ActiveSupport::TimeWithZone, Time, Date].each do |item|
   item.send(:include, CoreExt)
--- lib/core.rb
+++ lib/core.rb
@@ -1,26 +1,31 @@
 # encoding: utf-8
 
 #
-# All String, Fixnum, Float and DateTime class will be extended by this module
+# Fixnum, Float, String, Date, Time and DateTime class will be extended by this module
 #
 module CoreExt
 
   # Returns String
-  # Converts to Bangla digits only when applied to Fixnum, Float, String and DateTime object
+  # Converts to Bangla digits only when applied to Fixnum, Float, String, Date, Time and DateTime object
   # Example: "123".to_bddigit
   # "abc89efg".to_bddigit
-  # Deal.first.start_date.to_bddigit
+  # User.first.created_at.to_bddigit
   # 89.567.to_bddigit
   #
-  def to_ld(locale=:en)
+  def to_ld
     item = self
     digit_map = {
       :bn => {'0' => '০', '1' => '১', '2' => "২", '3' => '৩', '4' => '৪', '5' => '৫', '6' => '৬', '7' => '৭', '8' => '৮', '9' => '৯'},
       :en => {'0' => '0', '1' => '1', '2' => "2", '3' => '3', '4' => '4', '5' => '5', '6' => '6', '7' => '7', '8' => '8', '9' => '9'}
       }
     translated_item = ""
+    begin
+      locale = I18n.locale
+    rescue Exception => error
+      locale = :en
+    end
     item.to_s.split("").each do |c|
-      if c.to_i > 0 || c == '0'
+      if c.to_i > 0 || c == '0'        
         translated_item += digit_map[locale][c]
       else
         translated_item += c
